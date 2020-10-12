4ad53312470cae3d47eea65c3d896db0cd9d16f7
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-10-12 18:48:27 +0800

    api

---
+++ src/components/ui/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../../img/logo.png"
+    
+    
+const header = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt=""/>
+       </header>
+        
+    )
+}
+
+export default header
